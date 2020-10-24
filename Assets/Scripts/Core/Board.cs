6c81f34a71a7a8d0f32ce72e67cb8da5b139a802
reyhanlioglu <reyhanlioglu@itu.edu.tr>
2020-10-24 17:50:50 +0300

    ADD: Board created by hexagons is created

---
+++ Assets/Scripts/Core/Board.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public Transform mEmptySprite;
+
+    public int mHeight;
+    public int mWeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DrawEmptyCells();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DrawEmptyCells()
+    {
+        if(mEmptySprite != null)
+        {
+            for(int y = 0; y<mHeight; y++)
+            {
+                for(int x = 0; x<mWeight; x++)
+                {
+                        Transform clone;
+                        if(x % 2 == 0)
+                        {
+                            clone = Instantiate(mEmptySprite, new Vector3(x*2,y*2+1,0), Quaternion.identity) as Transform ;
+                        }
+                       else
+                       {
+                            clone = Instantiate(mEmptySprite, new Vector3(x*2,y*2,0), Quaternion.identity) as Transform ;
+                       }
+                        clone.name = "Board Space ( x = "+ x+ " , y = "+ y+ " )";
+                        clone.transform.parent = transform;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("WARNING! Assign the emptySprite object!");
+        }
+    }
+}
